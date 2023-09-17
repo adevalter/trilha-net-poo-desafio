2b383a301f83ed62469439f4ac4d940f7340039d
Adevalter <adevalter@hotmail.com>
2023-09-17 06:35:58 -0300

    Entrega resolução Desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,13 +4,23 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo;
+        protected string Imei;
+        protected int Memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, String imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
+        public void ModeloAparelho()
+        {
+           Console.WriteLine($"Smartphone: {Modelo} :");     
+        }
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "17 9-9999-9999", modelo:"Nokia C30", imei: "12897373939", memoria:128);
+nokia.ModeloAparelho();
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+Smartphone iphone = new Iphone(numero: "17 9-9888-8888", modelo:"Iphone 14", imei: "122972323939", memoria:256);
+iphone.ModeloAparelho();
+iphone.Ligar();
+iphone.InstalarAplicativo("Pintrest");
